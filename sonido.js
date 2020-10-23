a7eb3fd66ee57e02249b45c457b478de2e00a6c1
marco121942 <marco.sotomayor@tecsup.edu.pe>
2020-10-22 21:40:59 -0500

    aplicación sonido

--- sonido.js
+++ sonido.js
@@ -1,44 +1,39 @@
 window.addEventListener("load", () => {
-
-
-
-    const sonidos = document.querySelectorAll('.sonidos');
-
-    const pads = document.querySelectorAll('.pads div');
+    const sounds = document.querySelectorAll(".sonidos");
+    const pads = document.querySelectorAll(".pads div");
     const visual = document.querySelector(".visual");
     const colores = [
-        "lightcyan",
-        " blue",
-        "red",
-        "yellow",
-        "orange",
-        " black"
+
+
+        " #B2FF59",
+        " #69F0AE",
+        " #C8E6C9",
+        " #004D40",
+        " #006064",
+        " #424242"
+
     ];
 
-
     pads.forEach((pad, index) => {
-
         pad.addEventListener("click", function() {
-            sonidos[index].currentTime = 0;
-            sonidos[index].play();
+            sounds[index].currentTime = 0;
+            sounds[index].play();
             crearBolas(index);
-
-
         });
-
-
     });
 
 
-    const crearBolas = indexx => {
+    const crearBolas = index => {
 
         const bolas = document.createElement("div");
         visual.appendChild(bolas);
-        bolas.style.background = colores[index];
+        bolas.style.backgroundColor = colores[index];
+        bolas.style.animation = `jump 1s ease`;
+        bolas.addEventListener("animationend", function() {
+            visual.removeChild(this);
 
-        bolas.style.animation = `jump 1s ease`
+
+        })
+
     }
-
-
-
 });
